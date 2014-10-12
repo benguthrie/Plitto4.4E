b776d68e43636cd6b396ecca7e1fe6c33e60c7a6
Ben Guthrie <guthrie.ben+git@gmail.com>
2014-10-12 16:33:09 -0500

    Included some styles. Better file names
    File names are more clear now.

--- app/build/generated/source/r/debug/com/example/android/plitto/R.java
+++ app/build/generated/source/r/debug/com/example/android/plitto/R.java
@@ -138,31 +138,35 @@ public final class R {
         public static final int venus=0x7f020078;
     }
     public static final class id {
-        public static final int action_refresh=0x7f08000c;
-        public static final int action_websearch=0x7f08000b;
+        public static final int action_refresh=0x7f08000e;
+        public static final int action_websearch=0x7f08000d;
+        public static final int content=0x7f080008;
         public static final int content_frame=0x7f080001;
-        public static final int date=0x7f080009;
+        public static final int date=0x7f08000b;
         public static final int drawer_layout=0x7f080000;
         public static final int gridTitle=0x7f080004;
+        public static final int heading=0x7f080007;
         public static final int image=0x7f080006;
-        public static final int img=0x7f080008;
+        public static final int img=0x7f08000a;
         public static final int left_drawer=0x7f080002;
-        public static final int rowTextView=0x7f08000a;
-        public static final int txt=0x7f080007;
+        public static final int rowTextView=0x7f08000c;
+        public static final int txt=0x7f080009;
         public static final int userlist=0x7f080005;
         public static final int username=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int drawer_list_item=0x7f030001;
-        public static final int fragment_ditto_some=0x7f030009;
+        public static final int fragment_ditto_some=0x7f03000b;
         public static final int fragment_item_grid=0x7f030002;
         public static final int fragment_item_list=0x7f030003;
         public static final int fragment_planet=0x7f030004;
-        public static final int main_content=0x7f030005;
-        public static final int row_list=0x7f030006;
-        public static final int row_user=0x7f030007;
-        public static final int userrow=0x7f030008;
+        public static final int list_line=0x7f030005;
+        public static final int main_content=0x7f030006;
+        public static final int row_list=0x7f030007;
+        public static final int row_user=0x7f030008;
+        public static final int user_line=0x7f030009;
+        public static final int userrow=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f070000;
