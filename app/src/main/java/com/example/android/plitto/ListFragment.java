1f085f89cf0060051f261818ceb9d84fa6d53625
Ronak <ronak.mundra@hindustantimes.com>
2014-10-13 00:19:00 +0530

    listview onclick added for user and list

---
+++ app/src/main/java/com/example/android/plitto/ListFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.plitto;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by batman on 13/10/14.
+ */
+public class ListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_line,
+                container, false);
+        TextView tv = (TextView)view.findViewById(R.id.content);
+        tv.setText(getArguments().getString("username", ""));
+        return view;
+    }
+
+    public static ListFragment newInstance(String username) {
+        ListFragment fragmentDemo = new ListFragment();
+        Bundle args = new Bundle();
+        args.putString("username", username);
+        fragmentDemo.setArguments(args);
+        return fragmentDemo;
+    }
+}
